c1fd160a41a5011c4af98dab9c6052fe0f86a5c8
samarashabon <89557349+samarashabon@users.noreply.github.com>
2021-08-25 20:39:19 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***************************************
+ * Java Program Name
+****************************************
+*Samara Shabon
+*August 25, 2021
+*CMSC 255 Online & Asynchronous Section 
+****************************************/
+public class Hello {
+	public static void main (String[] args) {
+	System.out.println("Samara Shabon");
+	System.out.println("Java Program Name");
+	System.out.println("Lab 01");
+	System.out.println("August 25, 2021");
+	System.out.println("CMSC 255 Online & Asynchronous Section CRN 41688");
+	System.out.println();
+
+	}
+}
